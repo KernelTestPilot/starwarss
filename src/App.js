281159b2f185be176a1e87fca0ad4022f32fe63e
kerneltestdriver <oskardah@hotmail.com>
2023-01-26 11:44:26 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const API_URL = 'https://swapi.dev/api/';
 
 function App() {
+
+  //create a state for the app function
+const [searchTerm, setSearchTerm] = useState([]);
+
+//fetch function with api
+const fetchStarwars = async (type) => {
+const response = await fetch (`${API_URL}${type}`)
+const data = await response.json();
+//send data to useState
+setSearchTerm(data.results)
+}
+
+//runs last
+useEffect(() => {
+fetchStarwars()
+},[]);
+//render this bitch
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <div>
+      </div>
+      <button value = "people"  onClick={(e) => fetchStarwars(e.target.value)}> </button>
+      <button value = "planets" onClick={(e) => fetchStarwars(e.target.value)}> </button>
+      <button value = "films" onClick={(e) => fetchStarwars(e.target.value)}> </button>
+      <button value = "species" onClick={(e) => fetchStarwars(e.target.value)}> </button>
+      <button value = "vehicles" onClick={(e) => fetchStarwars(e.target.value)}> </button>
+      <button value = "starships" onClick={(e) => fetchStarwars(e.target.value)}> </button>
+     </div>
     </div>
   );
 }
